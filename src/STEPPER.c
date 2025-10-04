9008fe86d6d2ea37fba3468eba4b60c3b92555ff
MartH76 <martin25.husser@gmail.com>
2025-10-04 19:59:11 +0200

    update motor control

--- src/STEPPER.c
+++ src/STEPPER.c
@@ -1,28 +1,49 @@
 #include "PAMI_2026.h"
 
+StepperMotor motors[NUM_MOTORS];
 // ==============================
 // Init pins et moteurs
 // ==============================
 void init_motors() {
-    // for(int i = 0; i < NUM_MOTORS; i++) {
-    //     motors[i].pins = motor_pins[i];
-    //     motors[i].speed = 0;
-    //     motors[i].enabled = false;
-    //     motors[i].last_step_time_us = 0;
-    //     motors[i].step_period_us = 1000000; // par défaut très lent
+    motors[0].pins.step_pin = PIN_STEP_1;
+    motors[0].pins.dir_pin = PIN_DIR_1;
+    motors[0].pins.en_pin = 0; // disable pin
 
-    //     gpio_init(motors[i].pins.step_pin);
-    //     gpio_set_dir(motors[i].pins.step_pin, true);
+    // motors[1].pins.step_pin = PIN_STEP_2;
+    // motors[1].pins.dir_pin = PIN_DIR_2;
+    // motors[1].pins.en_pin = 0; // disable pin
 
-    //     gpio_init(motors[i].pins.dir_pin);
-    //     gpio_set_dir(motors[i].pins.dir_pin, true);
+    // motors[2].pins.step_pin = PIN_STEP_3;
+    // motors[2].pins.dir_pin = PIN_DIR_3;
+    // motors[2].pins.en_pin = 0; // disable pin
 
-    //     gpio_init(motors[i].pins.en_pin);
-    //     gpio_set_dir(motors[i].pins.en_pin, true);
-
-    //     // Désactiver le moteur au départ (LOW = enable pour A4988)
-    //     gpio_put(motors[i].pins.en_pin, true);
-    //     gpio_put(motors[i].pins.step_pin, false);
-    // }
+    for (int i = 0; i < NUM_MOTORS; i++) {
+        motors[i].current_position = 0;
+        motors[i].enabled = false;
+        motors[i].last_step_time_us = 0;
+        motors[i].step_period_us = 1000; // Default to 1000us (1ms) between steps
+    }
 }
 
+void Move_Loop(){
+    uint64_t current_time_us = Timer_us1;
+    for (int i=0; i<NUM_MOTORS; i++){
+        if(motors[i].enabled){
+            if(current_time_us - motors[i].last_step_time_us >= motors[i].step_period_us){
+                motors[i].last_step_time_us = current_time_us; // Update last step time
+                motors[i].step_pin = !motors[i].step_pin; // Toggle step pin
+                gpio_put(motors[i].pins.step_pin, motors[i].step_pin);
+                gpio_put(motors[i].pins.dir_pin, motors[i].dir_pin);
+                if(motors[i].step_pin){
+                    if(motors[i].dir_pin){
+                        motors[i].current_position++; // Move forward
+                    } else {
+                        motors[i].current_position--; // Move backward
+                    }
+                }
+            }
+        } else {
+            motors[i].last_step_time_us = current_time_us;
+        }
+    }
+}
--- src/STEPPER.h
+++ src/STEPPER.h
@@ -13,6 +13,8 @@ typedef struct {
     MotorPins pins;
     int32_t current_position; // Current position in steps
     bool enabled;
+    bool step_pin; // Current state of the step pin
+    bool dir_pin;  // Current state of the direction pin (0 or 1)
     uint64_t last_step_time_us; // Time of the last step in microseconds
     uint32_t step_period_us;  // Interval between steps in microseconds
 } StepperMotor;
